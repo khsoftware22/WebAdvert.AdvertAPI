da2eb32102a5acfb9311a524272bd396f6cecff3
khsoftware22 <66435597+khsoftware22@users.noreply.github.com>
2020-09-30 08:52:48 -0400

    initial commit

---
+++ AdvertAPI/Controllers/Advert.cs
@@ -0,0 +1,63 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using Amazon.DynamoDBv2;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [Route("api/v1/advert")]
+    [ApiController]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPost]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (Exception exception) 
+            {
+                return StatusCode(500, exception.Message);
+            }
+            return new OkResult();
+        }
+    }
+}
---
+++ AdvertAPI/Services/AdvertProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using AdvertApi.Models;
+
+namespace AdvertApi.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            CreateMap<AdvertModel, AdvertDbModel>();
+        }
+    }
+}
